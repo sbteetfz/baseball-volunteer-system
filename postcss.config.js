9036566603685c5ff2e666a7b5b8cccd41bd65ed
Stefanie Betz <stefaniebetz@Stef-MacBook-Pro.local>
2025-01-01 15:00:36 -0500

    Fix PostCSS configuration

--- postcss.config.js
+++ postcss.config.js
@@ -1,8 +1,8 @@
 const path = require('path')
 
 module.exports = {
-  plugins: [
-    'tailwindcss',
-    'autoprefixer',
-  ],
+  plugins: {
+    'tailwindcss': {},
+    'autoprefixer': {},
+  },
 }
